8ece0cd8092fd3a0e1948f8858808d811ea8c1d8
george ekow-daniels <ekowpo@yahoo.com>
2016-02-18 19:05:17 +0000

    add the sell and upgrade method to Tower Model and commented on the other class

--- src/com/app/towerDefense/guiComponents/JFileChooserComponent.java
+++ src/com/app/towerDefense/guiComponents/JFileChooserComponent.java
@@ -6,30 +6,34 @@ import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 import com.app.towerDefense.staticContent.AppilicationEnums.*;
+
 /**
  * This class allows us to select a file from system.
  * 
- * @author Sajjad Ashraf 
+ * @author Sajjad Ashraf
  *
  */
 public class JFileChooserComponent {
 
-	public JFileChooser getJFileChooser(E_JFileChooserrMode new_fileChooserrMode)
-	{
+	/**
+	 * The Constructor
+	 * 
+	 * @param new_fileChooserMode
+	 *            the mode of a file open
+	 * @return a file
+	 */
+	public JFileChooser getJFileChooser(E_JFileChooserrMode new_fileChooserMode) {
 		JFileChooser fileChooser = new JFileChooser();
 		fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
-		if(new_fileChooserrMode == E_JFileChooserrMode.Open)
-		{
+		if (new_fileChooserMode == E_JFileChooserrMode.Open) {
 			fileChooser.setDialogTitle("Tower Defense Select .tdm file");
-		}
-		else
-		{
+		} else {
 			fileChooser.setDialogTitle("Tower Defence Map Save");
 		}
 		fileChooser.setAcceptAllFileFilterUsed(false);
 		fileChooser.addChoosableFileFilter(new FileNameExtensionFilter("Tower Defence Map", "tdm"));
 		fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-		
+
 		return fileChooser;
 	}
 }
