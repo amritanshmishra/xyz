c6d063e4287419863423a1dfa308a6a658e33c93
amritansh <amishra2804@gmail.com>
2016-02-18 11:49:51 -0500

    Updating comments on TowerModelTestSuite

--- src/com/app/towerDefense/test/TowerModelTestSuite.java
+++ src/com/app/towerDefense/test/TowerModelTestSuite.java
@@ -3,16 +3,18 @@ package com.app.towerDefense.test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import com.app.towerDefense.models.TowerModel5;
 
+/**
+ * The class is a test suite for all the tower model test cases.
+ *
+ *
+ * @author Amritansh Mishra
+ * @version $Revision:
+ */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({
-	TowerModel1Test.class,
-	TowerModel2Test.class,
-	TowerModel3Test.class,
-	TowerModel4Test.class,
-	TowerModel5Test.class
-})
+@Suite.SuiteClasses({ TowerModel1Test.class, TowerModel2Test.class, TowerModel3Test.class, TowerModel4Test.class,
+		TowerModel5Test.class })
 public class TowerModelTestSuite {
 
-	
 }
