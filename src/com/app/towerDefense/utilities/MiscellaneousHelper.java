2e5e2b094e513ea145b7bf8080bf602d1ee42bd0
amritansh <amishra2804@gmail.com>
2016-02-18 17:56:33 -0500

    MiscellaneousHelper - added comments and java doc

--- src/com/app/towerDefense/utilities/MiscellaneousHelper.java
+++ src/com/app/towerDefense/utilities/MiscellaneousHelper.java
@@ -2,30 +2,73 @@ package com.app.towerDefense.utilities;
 
 import org.apache.commons.codec.binary.Base64;
 
+/**
+ * this class is a miscellaneous helper class
+ * 
+ * @author Sajjad Ashraf
+ *
+ */
 public class MiscellaneousHelper {
 
-	public String RemoveCharacterFromStrartorLeft(String InputString,
-			String Characters) {
+	/**
+	 * this method removes character from start to left
+	 * 
+	 * @param InputString
+	 *            input string
+	 * @param Characters
+	 *            characters to be removed
+	 * @return string the result
+	 */
+	public String RemoveCharacterFromStrartorLeft(String InputString, String Characters) {
 		return InputString.replaceAll("^\\" + Characters + "+", "");
 	}
 
-	public String RemoveCharacterFromEndorRight(String InputString,
-			String Characters) {
+	/**
+	 * the method removes character from end or right
+	 * 
+	 * @param InputString
+	 *            input string
+	 * @param Characters
+	 *            characters to be removed
+	 * @return string the result
+	 */
+	public String RemoveCharacterFromEndorRight(String InputString, String Characters) {
 		return InputString.replaceAll("\\" + Characters + "+$", "");
 	}
 
-	public String RemoveCharacterFromBothEnd(String InputString,
-			String Characters) {
-		return InputString.replaceAll("^\\" + Characters + "+|\\" + Characters
-				+ "+$", "");
+	/**
+	 * this method removes character from both end
+	 * 
+	 * @param InputString
+	 *            input string
+	 * @param Characters
+	 *            characters to be removed
+	 * @return string the result
+	 */
+	public String RemoveCharacterFromBothEnd(String InputString, String Characters) {
+		return InputString.replaceAll("^\\" + Characters + "+|\\" + Characters + "+$", "");
 	}
-	
+
+	/**
+	 * this method encodes the input to base64
+	 * 
+	 * @param input
+	 *            string
+	 * @return returns encoded string
+	 */
 	public String EncodeBase64(String input) {
 		byte[] bytesEncoded = Base64.encodeBase64(input.getBytes());
 		return new String(bytesEncoded);
 
 	}
 
+	/**
+	 * this method decodes the string from base64 encoding
+	 * 
+	 * @param input
+	 *            string base64
+	 * @return string the result
+	 */
 	public String DecodeBase64(String input) {
 		// Decrypt data on other side, by processing encoded data
 		byte[] valueDecoded = Base64.decodeBase64(input.getBytes());
