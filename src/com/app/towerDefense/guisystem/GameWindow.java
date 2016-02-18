8658409729c9b86ef836384c569051742c4cc252
george ekow-daniels <ekowpo@yahoo.com>
2016-02-18 04:36:47 +0000

    Java doc for Game Window and change of parameters name to meet coding conventions

--- src/com/app/towerDefense/guisystem/GameWindow.java
+++ src/com/app/towerDefense/guisystem/GameWindow.java
@@ -10,28 +10,39 @@ import com.app.towerDefense.guiComponents.JMenuBarComponent;
 
 /**
  * 
+ * This class creates the game main frame and initialise GUI
+ * 
  * @author usbaitass
- *
- *         
  *
  */
 public class GameWindow extends JFrame {
 
 	private JMenuBar gameJMenuBar;
 
-	// -- WINDOW Constructor
-	public GameWindow(int width, int height, String title, Game game) {
+	/**
+	 * WINDOW Constructor
+	 * 
+	 * @param new_width
+	 *            the width of the frame
+	 * @param new_height
+	 *            the height of the frame
+	 * @param new_title
+	 *            the title of the game
+	 * @param new_game
+	 *            the game main class
+	 */
+	public GameWindow(int new_width, int new_height, String new_title, Game new_game) {
 
-		this.setTitle(title);
-		this.setPreferredSize(new Dimension(width, height));
-		this.setMaximumSize(new Dimension(width, height));
-		this.setMinimumSize(new Dimension(width, height));
+		this.setTitle(new_title);
+		this.setPreferredSize(new Dimension(new_width, new_height));
+		this.setMaximumSize(new Dimension(new_width, new_height));
+		this.setMinimumSize(new Dimension(new_width, new_height));
 		this.setResizable(false);
 		this.setLocationRelativeTo(null); // center window on the screen
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		// -- initialize GUI of the Game
-		initGUI(width, height);
+		initGUI();
 
 		this.setVisible(true);
 		// this.add(game);
@@ -39,8 +50,10 @@ public class GameWindow extends JFrame {
 		// game.start();
 	}
 
-	// -- initialize GUI of the Game
-	public void initGUI(int width, int height) {
+	/**
+	 * This method initialise the GUI
+	 */
+	public void initGUI() {
 		this.getContentPane().removeAll();
 		this.setLayout(new BorderLayout());
 
@@ -48,5 +61,5 @@ public class GameWindow extends JFrame {
 		this.setJMenuBar(gameJMenuBar);
 	}
 
-	// END
+	
 }
