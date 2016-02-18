469a93fd7c00a00cacd68bc771dc435b00555689
amritansh <amishra2804@gmail.com>
2016-02-18 17:47:30 -0500

    FileStorage - Added comments and javadoc

--- src/com/app/towerDefense/utilities/FileStorage.java
+++ src/com/app/towerDefense/utilities/FileStorage.java
@@ -12,15 +12,24 @@ import com.google.gson.Gson;
 /**
  * This class is for saving a map
  * 
- * @author Sajjad 
+ * @author Sajjad
  *
  */
 public class FileStorage {
 
-	public String saveMapFile(File file, MapModel mapModel) {
-		String fileContent = getJsonFromObject(mapModel);
+	/**
+	 * this method saves the map file
+	 * 
+	 * @param new_file
+	 *            creates a new file object
+	 * @param new_mapModel
+	 *            creates a new map model
+	 * @return string message
+	 */
+	public String saveMapFile(File new_file, MapModel new_mapModel) {
+		String fileContent = getJsonFromObject(new_mapModel);
 		fileContent = (new MiscellaneousHelper()).EncodeBase64(fileContent);
-		String filePath = file.getPath();
+		String filePath = new_file.getPath();
 		if (!filePath.endsWith(".tdm"))
 			filePath += ".tdm";
 		try {
@@ -34,28 +43,51 @@ public class FileStorage {
 			return "ERROR : " + e.getMessage();
 		}
 	}
-	
-	public MapModel openMapFile(File file) {
-		
-		String fileContent ="";
-		 try {
-			 fileContent = new String(Files.readAllBytes(Paths.get(file.getPath())));
-			 fileContent = (new MiscellaneousHelper()).DecodeBase64(fileContent);
-			 return (MapModel) getObjectFromJson(fileContent, MapModel.class);
+
+	/**
+	 * this method opens up a map file
+	 * 
+	 * @param new_file
+	 *            new file object
+	 * @return mapModel model of the map
+	 */
+	public MapModel openMapFile(File new_file) {
+
+		String fileContent = "";
+		try {
+			fileContent = new String(Files.readAllBytes(Paths.get(new_file.getPath())));
+			fileContent = (new MiscellaneousHelper()).DecodeBase64(fileContent);
+			return (MapModel) getObjectFromJson(fileContent, MapModel.class);
 		} catch (IOException e) {
 			e.printStackTrace();
-			return  null;
-		}		
+			return null;
+		}
 	}
 
-	public String getJsonFromObject(Object object) {
+	/**
+	 * this method gets json from object
+	 * 
+	 * @param new_object
+	 *            new object
+	 * @return json converts gson to json and returns it
+	 */
+	public String getJsonFromObject(Object new_object) {
 		Gson gson = new Gson();
-		return gson.toJson(object);
+		return gson.toJson(new_object);
 	}
 
-	public Object getObjectFromJson(String jsonString, Class<?> _class) {
+	/**
+	 * this methods gets object from a json
+	 * 
+	 * @param new_jsonString
+	 *            json string object
+	 * @param new_class
+	 *            new clas
+	 * @return object object from json
+	 */
+	public Object getObjectFromJson(String new_jsonString, Class<?> new_class) {
 		Gson gson = new Gson();
-		return gson.fromJson(jsonString, _class);
+		return gson.fromJson(new_jsonString, new_class);
 	}
 
 }
