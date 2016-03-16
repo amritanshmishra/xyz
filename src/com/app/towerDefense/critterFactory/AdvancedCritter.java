f85ea2fb5fe94d2e9f6ddb839e3f83181273da44
amritansh <amishra2804@gmail.com>
2016-03-16 14:29:05 -0400

    Updated advancedcritter and basiccritter class

--- src/com/app/towerDefense/critterFactory/AdvancedCritter.java
+++ src/com/app/towerDefense/critterFactory/AdvancedCritter.java
@@ -2,7 +2,13 @@ package com.app.towerDefense.critterFactory;
 
 import java.awt.Image;
 
-@SuppressWarnings("serial")
+/**
+ * This class is the advanced critter that would move on the map Contains
+ * different methhods to create and move a critter on the map selected by player
+ * 
+ * @author Amritansh Mishra
+ *
+ */
 public class AdvancedCritter implements CritterType {
 
 	/**
@@ -10,7 +16,7 @@ public class AdvancedCritter implements CritterType {
 	 */
 	private int x, y;
 	public int movePoint;
-	public static int moveStandard = 0;
+
 	/**
 	 * Id of critter
 	 */
@@ -29,76 +35,101 @@ public class AdvancedCritter implements CritterType {
 	public int currentHealth;
 
 	/**
-	 * 
+	 * Getter method for x co-ordinate of critter
 	 */
-	public int getX(){
+	public int getX() {
 		return x;
 	}
-	
+
 	/**
-	 * 
+	 * Getter method for y co-ordinate of critter
 	 */
-	public int getY(){
+	public int getY() {
 		return y;
 	}
 
-	
+	/**
+	 * Default constructor for advanced critter
+	 */
 	public AdvancedCritter() {
 	}
 
-
+	/**
+	 * Get actual health of the critter
+	 */
 	@Override
 	public int getActualHealth() {
 
 		return 0;
 	}
 
+	/**
+	 * Returns the current health of the critter
+	 */
 	@Override
 	public int getCurrentHealth() {
 
 		return 0;
 	}
 
+	/**
+	 * Setter method for actual critter health
+	 */
 	@Override
 	public void setActualHealth(int actualHealth) {
 
 	}
 
+	/**
+	 * Setter method for current critter health
+	 */
 	@Override
 	public void setCurrentHealth(int currentHealth) {
 
 	}
 
+	/**
+	 * Returns critter id
+	 */
 	@Override
 	public int getCritterId() {
 
 		return 0;
 	}
 
-
+	/**
+	 * Calculates the path on which the critter would move
+	 */
 	@Override
 	public void calculatePath() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-
+	/**
+	 * This method sets the button(blocks) parameters
+	 */
 	@Override
 	public void setBlocksParams(int new_w, int new_h) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-
+	/**
+	 * This method returns the image used for the critter
+	 */
 	@Override
 	public Image getCritterImage() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * This method sets the x and y co-ordinate for the critter
+	 */
 	@Override
 	public void setXY(int new_x, int new_y) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
--- src/com/app/towerDefense/critterFactory/BasicCritter.java
+++ src/com/app/towerDefense/critterFactory/BasicCritter.java
@@ -6,21 +6,25 @@ import javax.swing.ImageIcon;
 
 import com.app.towerDefense.staticContent.ApplicationStatics;
 
-@SuppressWarnings("serial")
+/**
+ * This class is the basic critter that would move on the map Contains different
+ * methhods to create and move a critter on the map selected by player
+ * 
+ * @author Amritansh Mishra
+ *
+ */
 public class BasicCritter implements CritterType {
 
-	
 	/**
 	 * current X and Y position of critter
 	 */
 	public int x, y;
 	public int directionX;
 	public int directionY;
-	public static int moveStandard = 0;
 	private int i = 0;
 	private int blockW, blockH;
 	public Image image;
-	
+
 	/**
 	 * Id of critter
 	 */
@@ -38,86 +42,114 @@ public class BasicCritter implements CritterType {
 	 */
 	public int currentHealth;
 
-
-
+	/**
+	 * Constructor to initialize the critter with image
+	 */
 	public BasicCritter() {
 		image = new ImageIcon("images/critter1.gif").getImage();
 	}
 
-
+	/**
+	 * Gets the x co-ordinate for critter
+	 */
 	@Override
 	public int getX() {
 
 		return x;
 	}
 
+	/**
+	 * Gets the y co-ordinate for critter
+	 */
 	@Override
 	public int getY() {
 
 		return y;
 	}
 
+	/**
+	 * Get actual health of the critter
+	 */
 	@Override
 	public int getActualHealth() {
 
 		return 0;
 	}
 
+	/**
+	 * Returns the current health of the critter
+	 */
 	@Override
 	public int getCurrentHealth() {
 
 		return 0;
 	}
 
+	/**
+	 * Setter method for actual critter health
+	 */
 	@Override
 	public void setActualHealth(int actualHealth) {
 
 	}
 
+	/**
+	 * Setter method for current critter health
+	 */
 	@Override
 	public void setCurrentHealth(int currentHealth) {
 
 	}
 
+	/**
+	 * Returns critter id
+	 */
 	@Override
 	public int getCritterId() {
 
 		return 0;
 	}
-	
+
+	/**
+	 * Calculates the path on which the critter would move
+	 */
 	@Override
 	public void calculatePath() {
 
-		
 		directionY = ApplicationStatics.PATH_ARRAY1.get(i + 1).x - ApplicationStatics.PATH_ARRAY1.get(i).x;
-		directionX = ApplicationStatics.PATH_ARRAY1.get(i + 1).y- ApplicationStatics.PATH_ARRAY1.get(i).y;
+		directionX = ApplicationStatics.PATH_ARRAY1.get(i + 1).y - ApplicationStatics.PATH_ARRAY1.get(i).y;
 
 		x += directionX;
 		y += directionY;
-		
-		if (x >= ApplicationStatics.PATH_ARRAY1.get(i+1).y* blockW && y >= ApplicationStatics.PATH_ARRAY1.get(i+1).x * blockH) {
-				i++;
+
+		if (x >= ApplicationStatics.PATH_ARRAY1.get(i + 1).y * blockW
+				&& y >= ApplicationStatics.PATH_ARRAY1.get(i + 1).x * blockH) {
+			i++;
 		}
-		System.out.println("inside critter   x : "+x+" , y : "+y);
+		System.out.println("inside critter   x : " + x + " , y : " + y);
 
 	}
-	
+
 	/**
 	 * This method sets the button(blocks) parameters
 	 */
-	public void setBlocksParams(int new_w, int new_h){
+	public void setBlocksParams(int new_w, int new_h) {
 		blockW = new_w;
 		blockH = new_h;
 	}
 
-
+	/**
+	 * This method returns the image used for the critter
+	 */
 	@Override
 	public Image getCritterImage() {
 		// TODO Auto-generated method stub
 		return image;
 	}
 
-
+	/**
+	 * This method sets the x and y co-ordinate for the critter
+	 */
 	@Override
 	public void setXY(int new_x, int new_y) {
 		// TODO Auto-generated method stub
