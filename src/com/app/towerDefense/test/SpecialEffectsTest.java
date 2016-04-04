3d47e40ef6d405898f1a6d3ff07276e991d71fb7
usbaitass <usbaitass@localhost>
2016-04-04 01:05:03 -0400

    more of javadoc and comment

--- src/com/app/towerDefense/test/SpecialEffectsTest.java
+++ src/com/app/towerDefense/test/SpecialEffectsTest.java
@@ -7,33 +7,51 @@ import org.junit.*;
 import com.app.towerDefense.guiComponents.MapPanel;
 import com.app.towerDefense.models.BasicCritter;
 import com.app.towerDefense.models.MapModel;
-
+/**
+ * This Test class checks if the Special Effects that were implemented in the project work as intended 
+ * @author usbaitass
+ *
+ */
 public class SpecialEffectsTest {
 
 	BasicCritter critter;
-	
+	/**
+	 * Runs before all test cases
+	 */
 	@BeforeClass
 	public static void beforeClass() {
 		System.out.println("Entered Testing SpecialEffectsTest Class");
 	}
 
+	/**
+	 * Runs after all test cases are completed
+	 */
 	@AfterClass
 	public static void AfterClass() {
 		System.out.println("Left Testing SpecialEffectsTest Class");
 	}
 
+	/**
+	 * Initialize the variables before each test case
+	 */
 	@Before
 	public void before() {
 		critter = new BasicCritter(1);
 		System.out.print("inside");
 	}
 
+	/**
+	 * Delete variables after each test case execution
+	 */
 	@After
 	public void after() {
 		critter = null;
 		System.out.println("outside");
 	}
 
+	/**
+	 * Test case that checks if the freezing towers slow effect works properly
+	 */
 	@Test
 	public void testSlowSpeed() {
 		System.out.println(" testSlowSpeed()");
@@ -46,6 +64,9 @@ public class SpecialEffectsTest {
 
 	}
 
+	/**
+	 * Test case that checks if burning towers burn effect works properly
+	 */
 	@Test
 	public void testBurnHealth() {
 		System.out.println(" testBurnHealth()");
@@ -70,6 +91,9 @@ public class SpecialEffectsTest {
 		}
 	}
 
+	/**
+	 * Test case that checks if splash damage is applied properly
+	 */
 	@Test
 	public void testSplashDamage() {
 		System.out.println(" testSplashDamage()");
--- src/com/app/towerDefense/test/StrategyTest.java
+++ src/com/app/towerDefense/test/StrategyTest.java
@@ -2,10 +2,6 @@ package com.app.towerDefense.test;
 
 import static org.junit.Assert.*;
 
-import java.awt.Graphics;
-
-import javax.swing.JPanel;
-
 import org.junit.*;
 
 import org.junit.Test;
@@ -19,22 +15,37 @@ import com.app.towerDefense.models.Shooter;
 import com.app.towerDefense.models.StrongestStrategy;
 import com.app.towerDefense.models.WeakestStrategy;
 
+/**
+ * This class test 4 strategies that were implemented in the project
+ * 
+ * @author usbaitass
+ *
+ */
 public class StrategyTest {
 
 	Shooter tower;
 	BasicCritter critter1;
 	BasicCritter critter2;
 
+	/**
+	 * This method runs the before all test cases
+	 */
 	@BeforeClass
 	public static void beforeClass() {
 		System.out.println("Entered StrategyTest Class");
 	}
 
+	/**
+	 * This method runs after all test cases were ran
+	 */
 	@AfterClass
 	public static void afterClass() {
 		System.out.println("Left StrategyTest Class");
 	}
 
+	/**
+	 * This method initiate the variable before each test case
+	 */
 	@Before
 	public void before() {
 		System.out.print("inside");
@@ -47,18 +58,22 @@ public class StrategyTest {
 		tower.towerID = 1;
 
 		tower.setXY(10, 10);
-
 	}
 
+	/**
+	 * This method deletes and sets variables to null after each test case
+	 */
 	@After
 	public void after() {
 		System.out.println("outside");
 		tower = null;
 		critter1 = null;
 		critter2 = null;
-
 	}
 
+	/**
+	 * Test case that checks if the Strongest Strategy works as intented
+	 */
 	@Test
 	public void testStrongestStrategy() {
 		System.out.println(" testStrongestStrategy()");
@@ -111,6 +126,9 @@ public class StrategyTest {
 		}
 	}
 
+	/**
+	 * This test case checks if Weakest strategy works as intended
+	 */
 	@Test
 	public void testWeakestStrategy() {
 		System.out.println(" testWeakestStrategy()");
@@ -163,6 +181,9 @@ public class StrategyTest {
 		}
 	}
 
+	/**
+	 * This test case checks if Nearest to tower strategy works as intended
+	 */
 	@Test
 	public void testNearestToTowerStrategy() {
 		System.out.println(" testNearestToTowerStrategy()");
@@ -256,6 +277,9 @@ public class StrategyTest {
 		}
 	}
 
+	/**
+	 * This test case checks if Nearest to End point strategy works as intended
+	 */
 	@Test
 	public void testNearestToEndPointStrategy() {
 		System.out.println(" testNearestToEndPointStrategy()");
@@ -277,19 +301,19 @@ public class StrategyTest {
 		assertEquals("NearToEND", tower.getStrategy().getStrategyName());
 
 		// Here we place the critters on the map path blocks
-		tower.setXY(0, 0);	
-		critter1.setXY(20,0);
-		critter2.setXY(30,0);
-		critter3.setXY(100,0);
-		
+		tower.setXY(0, 0);
+		critter1.setXY(20, 0);
+		critter2.setXY(30, 0);
+		critter3.setXY(100, 0);
+
 		// place the critter inside path blocks which
 		// ordered ascending, which represented as
 		// from Entry to Exit block
 		critter1.currentBlocki = 1;
 		critter2.currentBlocki = 1;
 		critter3.currentBlocki = 2;
-	//	critter3.currentBlocki = 1;
-		
+		// critter3.currentBlocki = 1;
+
 		// we indicate the direction in which critters are moving
 		critter1.directionX = 1;
 		critter1.directionY = 0;
@@ -297,7 +321,6 @@ public class StrategyTest {
 		critter2.directionY = 0;
 		critter3.directionX = 1;
 		critter3.directionY = 0;
-		
 
 		try {
 			Thread thread = new Thread();
@@ -316,13 +339,12 @@ public class StrategyTest {
 			tower.executeStrategy(tower, critter1);
 			tower.executeStrategy(tower, critter2);
 			tower.executeStrategy(tower, critter3);
-			
+
 			// check if expected data is the same
 			assertEquals(9, critter1.getCurrentHealth());
 			assertEquals(10, critter2.getCurrentHealth());
 			assertEquals(9, critter3.getCurrentHealth());
-			
-	
+
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
